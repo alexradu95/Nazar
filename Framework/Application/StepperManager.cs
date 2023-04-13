d880cc54379763b5fcf48a915065f70467154132
AlexRadu <radualexandrucosmin@gmail.com>
2023-04-13 14:11:05 +0300

    Missed commits files

--- Framework/Application/StepperManager.cs
+++ Framework/Application/StepperManager.cs
@@ -1,25 +1,23 @@
-﻿using Framework.Application.Interfaces;
+﻿using System.Collections.ObjectModel;
+using Framework.Application.Interfaces;
 using StereoKit;
 using StereoKit.Framework;
-using System.Collections.ObjectModel;
 
-namespace LauncherMultiPlatform
+namespace LauncherMultiPlatform;
+
+public class StepperManager : IStepperManager
 {
-    public class StepperManager : IStepperManager
+    private readonly List<IStepper> registeredSteppers = new();
+
+    public ReadOnlyCollection<IStepper> SteppersList => registeredSteppers.AsReadOnly();
+
+    public void RegisterStepper<T>() where T : IStepper
     {
+        registeredSteppers.Add(SK.AddStepper<T>());
+    }
 
-        private readonly List<IStepper> registeredSteppers = new();
-
-        public ReadOnlyCollection<IStepper> SteppersList => registeredSteppers.AsReadOnly();
-
-        public void RegisterStepper<T>() where T : IStepper
-        {
-            registeredSteppers.Add(SK.AddStepper<T>());
-        }
-
-        public void RegisterStepper<T>(T stepper) where T : IStepper
-        {
-            registeredSteppers.Add(SK.AddStepper(stepper));
-        }
+    public void RegisterStepper<T>(T stepper) where T : IStepper
+    {
+        registeredSteppers.Add(SK.AddStepper(stepper));
     }
 }
--- Framework/SceneGraph/Behaviours/EntityContainer.cs
+++ Framework/SceneGraph/Behaviours/EntityContainer.cs
@@ -1,30 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Framework.SceneGraph.Interfaces;
+﻿using Framework.SceneGraph.Interfaces;
 
-namespace Framework.SceneGraph.Behaviours
+namespace Framework.SceneGraph.Behaviours;
+
+internal class EntityContainer : IEntityContainer
 {
-    internal class EntityContainer : IEntityContainer
+    public List<IEntity> Entities { get; } = new();
+
+    public void AddEntity(IEntity entity)
     {
-        public List<IEntity> Entities { get; } = new();
+        Entities.Add(entity);
+        OnEntitiesListChange(entity, true);
+    }
 
-        public void AddEntity(IEntity entity)
-        {
-            Entities.Add(entity);
-            OnEntitiesListChange(entity, true);
-        }
+    public void RemoveEntity(IEntity entity)
+    {
+        Entities.Remove(entity);
+        OnEntitiesListChange(entity, false);
+    }
 
-        public void RemoveEntity(IEntity entity)
-        {
-            Entities.Remove(entity);
-            OnEntitiesListChange(entity, false);
-        }
-
-        protected virtual void OnEntitiesListChange(IEntity entity, bool wasAdded)
-        {
-        }
+    protected virtual void OnEntitiesListChange(IEntity entity, bool wasAdded)
+    {
     }
 }
