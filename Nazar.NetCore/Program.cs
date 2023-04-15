aed5246bb6d5b92252260d655d3d7e1f7e25a508
Radu Alexandru <alexradu95@proton.me>
2023-04-15 13:58:38 +0300

    Added netcore launcher project

---
+++ Nazar.NetCore/Program.cs
@@ -0,0 +1,18 @@
+﻿using Nazar;
+using StereoKit;
+using System;
+
+App app = new App();
+
+app.PreInit();
+
+if (!SK.Initialize(app.Settings))
+    Environment.Exit(1);
+
+app.Init();
+
+// Core application loop
+SK.Run(() =>
+{
+    app.Step();
+});
